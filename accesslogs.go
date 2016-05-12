2b432f1e10f80002268de7fc56e1f27122390d43
Laurence Morgan <laurence@gfm.co.uk>
2016-05-12 12:23:15 +0100

    bugfix: Coyp & paste mistakes https://github.com/lmorg/apachelogs/issues/1

--- accesslogs.go
+++ accesslogs.go
@@ -166,43 +166,43 @@ func (sal Sort) Sort()            { sort.Sort(sal) }
 func (sal Sort) Less(i, j int) bool {
 	switch sal.Key {
 	case FIELD_IP:
-		return (*sal.AccessLogs)[i].IP < (*sal.AccessLogs)[i].IP
+		return (*sal.AccessLogs)[i].IP < (*sal.AccessLogs)[j].IP
 
 	case FIELD_USER_ID:
-		return (*sal.AccessLogs)[i].UserID < (*sal.AccessLogs)[i].UserID
+		return (*sal.AccessLogs)[i].UserID < (*sal.AccessLogs)[j].UserID
 
 	case FIELD_DATE_TIME, FIELD_DATE, FIELD_TIME:
 		return (*sal.AccessLogs)[i].DateTime.Before((*sal.AccessLogs)[j].DateTime)
 
 	case FIELD_METHOD:
-		return (*sal.AccessLogs)[i].Method < (*sal.AccessLogs)[i].Method
+		return (*sal.AccessLogs)[i].Method < (*sal.AccessLogs)[j].Method
 
 	case FIELD_URI:
-		return (*sal.AccessLogs)[i].URI < (*sal.AccessLogs)[i].URI
+		return (*sal.AccessLogs)[i].URI < (*sal.AccessLogs)[j].URI
 
 	case FIELD_QUERY_STRING:
-		return (*sal.AccessLogs)[i].QueryString < (*sal.AccessLogs)[i].QueryString
+		return (*sal.AccessLogs)[i].QueryString < (*sal.AccessLogs)[j].QueryString
 
 	case FIELD_PROTOCOL:
-		return (*sal.AccessLogs)[i].Protocol < (*sal.AccessLogs)[i].Protocol
+		return (*sal.AccessLogs)[i].Protocol < (*sal.AccessLogs)[j].Protocol
 
 	case FIELD_STATUS:
-		return (*sal.AccessLogs)[i].Status.I < (*sal.AccessLogs)[i].Status.I
+		return (*sal.AccessLogs)[i].Status.I < (*sal.AccessLogs)[j].Status.I
 
 	case FIELD_SIZE:
-		return (*sal.AccessLogs)[i].Size < (*sal.AccessLogs)[i].Size
+		return (*sal.AccessLogs)[i].Size < (*sal.AccessLogs)[j].Size
 
 	case FIELD_REFERRER:
-		return (*sal.AccessLogs)[i].Referrer < (*sal.AccessLogs)[i].Referrer
+		return (*sal.AccessLogs)[i].Referrer < (*sal.AccessLogs)[j].Referrer
 
 	case FIELD_USER_AGENT:
-		return (*sal.AccessLogs)[i].UserAgent < (*sal.AccessLogs)[i].UserAgent
+		return (*sal.AccessLogs)[i].UserAgent < (*sal.AccessLogs)[j].UserAgent
 
 	case FIELD_PROC_TIME:
-		return (*sal.AccessLogs)[i].ProcTime < (*sal.AccessLogs)[i].ProcTime
+		return (*sal.AccessLogs)[i].ProcTime < (*sal.AccessLogs)[j].ProcTime
 
 	case FIELD_FILE_NAME:
-		return (*sal.AccessLogs)[i].FileName < (*sal.AccessLogs)[i].FileName
+		return (*sal.AccessLogs)[i].FileName < (*sal.AccessLogs)[j].FileName
 
 	case 0:
 		panic("Key unset on sort")
